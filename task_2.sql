a0eee82df472f0a9de02f88418abeb9e048be856
Nai Emmanuel Mollel <51174372+naimollel@users.noreply.github.com>
2025-10-20 08:38:43 +0300

    2. Create Your Magical Tables

--- task_2.sql
+++ task_2.sql
@@ -2,44 +2,31 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
--- Table: authors
-CREATE TABLE IF NOT EXISTS authors (
+-- Table: Authors
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Table: books
-CREATE TABLE IF NOT EXISTS books (
+-- Table: Books
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Table: customers
-CREATE TABLE IF NOT EXISTS customers (
+-- Table: Customers
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215),
     address TEXT
 );
 
--- Table: orders
-CREATE TABLE IF NOT EXISTS orders (
+-- Table: Orders
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-);
-
--- Table: order_details
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
-);
+    cus
